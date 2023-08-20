247ed47157bcc5ba56d05e0b8e7fec7d4b77c833
MazayKun <kir99916@gmail.com>
2023-08-20 19:56:18 +0300

    Добавил задание по jpql

--- L21-jpql/homework-template/src/main/java/ru/otus/crm/model/Client.java
+++ L21-jpql/homework-template/src/main/java/ru/otus/crm/model/Client.java
@@ -1,47 +1,58 @@
 package ru.otus.crm.model;
 
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
 @Entity
 @Table(name = "client")
 public class Client implements Cloneable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "client_id_custom_generator", allocationSize = 1)
+    @GeneratedValue(generator = "client_id_custom_generator")
     @Column(name = "id")
     private Long id;
 
     @Column(name = "name")
     private String name;
 
-    public Client(String name) {
-        this.id = null;
-        this.name = name;
+    @OneToOne(cascade = CascadeType.ALL)
+    private Address address;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private Set<Phone> phones = new HashSet<>();
+
+    public void addPhone(Phone phone) {
+        phones.add(phone);
+        phone.setClient(this);
     }
 
-    public Client(Long id, String name) {
-        this.id = id;
+    public Client(String name, Address address) {
         this.name = name;
+        this.address = address;
     }
 
     @Override
     public Client clone() {
-        return new Client(this.id, this.name);
-    }
-
-    @Override
-    public String toString() {
-        return "Client{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        Client clone = new Client();
+        clone.setId(this.id);
+        clone.setName(this.name);
+        clone.setAddress(this.address.clone());
+        clone.setPhones(this.getPhones().stream()
+                .map(phone -> phone.clone(clone))
+                .collect(Collectors.toSet()));
+        return clone;
     }
 }
---
+++ L21-jpql/homework-template/src/main/java/ru/otus/crm/model/Phone.java
@@ -0,0 +1,44 @@
+package ru.otus.crm.model;
+
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phone")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Phone {
+
+    @Id
+    @SequenceGenerator(name = "phone_id_custom_generator", allocationSize = 1)
+    @GeneratedValue(generator = "phone_id_custom_generator")
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "number")
+    private String number;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Client client;
+
+    public Phone(String number) {
+        this.number = number;
+    }
+
+    public Phone clone(Client client) {
+        return new Phone(this.id, this.number, client);
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                ", client_id=" + (client == null ? null : client.getId()) +
+                '}';
+    }
+}
